fc5d633477dfc07f4a173b3f58c2bb390d0f5d49
Lukasz Solka <solkalukasz@gmail.com>
2015-01-31 14:49:39 +0100

    generate Tasks scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :tasks
+
   root to: 'visitors#index'
   devise_for :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150126160928) do
+ActiveRecord::Schema.define(version: 20150131134556) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.boolean  "done"
+    t.string   "description", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
