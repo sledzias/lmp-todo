375bf920a74e139352e4c2f89365a19b3171fdbb
Szymon Borucki <szymon.borucki@gmail.com>
2015-01-31 13:36:58 +0800

    Serialized tasks and added api route

--- app/assets/javascripts/store.js.coffee
+++ app/assets/javascripts/store.js.coffee
@@ -1,8 +1,6 @@
-# Adds X-CSRF-Token to all REST requests.
-# Allows for the use of Rails protect_from_forgery
-# The CSRF Token is normally found in app/views/layouts/application.html.*
-# inserted with the rails helper: "csrf_meta_tags"
-DS.RESTAdapter.reopen(
-  headers:
-    "X-CSRF-Token": $('meta[name="csrf-token"]').attr('content')
-)
+
+App.ApplicationStore = DS.Store.extend()
+App.ApplicationAdapter = DS.ActiveModelAdapter.extend()
+
+DS.RESTAdapter.reopen
+  namespace: 'api/v1'
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,39 +1,39 @@
 class Task < ActiveRecord::Base
-  validates :description, presence: true
+  # validates :description, presence: true
 
-  def available?
-    self.done == false
-  end
+  # def available?
+  #   self.done == false
+  # end
 
-  def done?
-    self.done == true
-  end
+  # def done?
+  #   self.done == true
+  # end
 
-  def self.count_tasks(attr)
-    if attr == 'active'
-      where("done = ?", false).count
-    elsif attr == 'completed'
-      where("done = ?", true).count
-    elsif attr == 'all'
-      self.count
-    end
-  end
+  # def self.count_tasks(attr)
+  #   if attr == 'active'
+  #     where("done = ?", false).count
+  #   elsif attr == 'completed'
+  #     where("done = ?", true).count
+  #   elsif attr == 'all'
+  #     self.count
+  #   end
+  # end
 
-  def self.active_tasks
-    where("done = ?", false)
-  end
+  # def self.active_tasks
+  #   where("done = ?", false)
+  # end
 
-  def self.finished_tasks
-    where("done = ?", true)
-  end
+  # def self.finished_tasks
+  #   where("done = ?", true)
+  # end
 
-  def self.count_tasks(attr)
-    if attr == 'active'
-      where("done = ?", false).count
-    elsif attr == 'completed'
-      where("done = ?", true).count
-    elsif attr == 'all'
-      self.count
-    end
-  end
+  # def self.count_tasks(attr)
+  #   if attr == 'active'
+  #     where("done = ?", false).count
+  #   elsif attr == 'completed'
+  #     where("done = ?", true).count
+  #   elsif attr == 'all'
+  #     self.count
+  #   end
+  # end
 end
