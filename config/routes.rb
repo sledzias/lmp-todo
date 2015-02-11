b5493f2ef5bff4e7ea4b7824dc2b6ab53cdbcfd8
Pawel Michna <pmichna@gmail.com>
2015-02-12 00:37:49 +0100

    wciśnięcie krzyżyka usuwa zadanie

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/tasks', to: 'visitors#index'
   get '/tasks/:id/toggle', to: 'tasks#toggle_done', as: 'task_toggle'
-  resources :tasks, only: :create
+  resources :tasks, only: [:create, :destroy]
   root to: 'visitors#index'
   devise_for :users
 end
